c4003bada5b4d4694bbb72602e3b2a612a367659
elizabeth-agada <elizadave6@gmail.com>
2026-06-07 18:33:42 +0100

    fix the clerk middleware

--- snipify/middleware.ts
+++ snipify/middleware.ts
@@ -1,22 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+])
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth()
-
-  if (isProtectedRoute(req) && !userId) {
-    return NextResponse.redirect(new URL('/sign-in', req.url))
-  }
-
-  if (userId && (req.nextUrl.pathname === '/sign-in' || req.nextUrl.pathname === '/sign-up')) {
-    return NextResponse.redirect(new URL('/dashboard', req.url))
+  if (isProtectedRoute(req)) {
+    await auth.protect()
   }
 })
 
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)', '/(api|trpc)(.*)'],
 }
